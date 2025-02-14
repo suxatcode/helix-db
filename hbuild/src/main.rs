6599487068d112c6b2b1590b13ffda55cf92eb15
xavcochran <le22092@bristol.ac.uk>
2025-02-14 11:42:54 +0000

    just starting

---
+++ hbuild/src/main.rs
@@ -0,0 +1,12 @@
+// make sure build is run in sudo mode
+fn main() {
+    // run server on specified port
+
+    // on requests, stop systemctl service
+
+    // pull latest query file from s3
+
+    // run lint and compile queries
+
+    // start systemctl service
+}
