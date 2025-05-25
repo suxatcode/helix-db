d454abbd55162e882584ccfba9a1df7faf5f6481
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2025-05-25 10:45:30 +0200

    pull from branch

--- helix-cli/src/main.rs
+++ helix-cli/src/main.rs
@@ -693,6 +693,8 @@ fn main() {
 
             let mut runner = Command::new("git");
             runner.arg("clone");
+            runner.arg("--branch");
+            runner.arg("cli-fixes");
             runner.arg("https://github.com/HelixDB/helix-db.git");
             runner.current_dir(&repo_path);
 
