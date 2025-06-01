86f6b2961c8b3568331e4f996bf53b72be95d48d
xav-db <xav@helix-db.com>
2025-05-31 23:47:57 -0700

    updating to return from search

--- helixdb/src/helix_engine/vector_core/vector_core.rs
+++ helixdb/src/helix_engine/vector_core/vector_core.rs
@@ -230,7 +230,7 @@ impl VectorCore {
             arr[..len].copy_from_slice(&ep_id[..len]);
 
             let ep = self
-                .get_vector(txn, u128::from_be_bytes(arr), 0, true)
+                .get_vector(txn, u128::from_be_bytes(arr), 0, false)
                 .map_err(|_| VectorError::EntryPointNotFound)?;
             Ok(ep)
         } else {
@@ -292,7 +292,7 @@ impl VectorCore {
                 let neighbor_id = u128::from_be_bytes(arr);
 
                 if neighbor_id != id {
-                    if let Ok(vector) = self.get_vector(txn, neighbor_id, level, true) {
+                    if let Ok(vector) = self.get_vector(txn, neighbor_id, level, false) {
                         // TODO: look at implementing a macro that actually just runs each function rather than iterating through
                         if filter.is_none() || filter.unwrap().iter().all(|f| f(&vector, txn)) {
                             neighbors.push(vector);
@@ -530,7 +530,19 @@ impl HNSW for VectorCore {
             },
         )?;
 
-        Ok(candidates.to_vec_with_filter(k, filter, txn))
+        let mut results = candidates.to_vec_with_filter(k, filter, txn);
+
+        for result in results.iter_mut() {
+            result.properties = match self
+                .vector_data_db
+                .get(txn, &result.get_id().to_be_bytes())?
+            {
+                Some(bytes) => Some(bincode::deserialize(&bytes).map_err(VectorError::from)?),
+                None => None, // Maybe should be an error?
+            };
+        }
+
+        Ok(results)
     }
 
     fn insert<F>(
